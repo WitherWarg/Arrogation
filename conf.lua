61e160b6bd43a6660603a05ea2bf8bd6cee599ef
WitherWarg <ykhemiss@gmail.com>
2024-05-28 21:17:03 -0400

    Added automatic map clamping

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.window.title = "Game"
+    t.window.resizable = true
+    t.window.fullscreen = true
     -- t.window.icon = "icon.png"
-    -- t.window.resizable = true
-    -- t.window.fullscreen = true
 end
